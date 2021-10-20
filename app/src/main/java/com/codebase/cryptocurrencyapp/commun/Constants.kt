7b29bc313f7e24b9c0c47b2f3444b7a92d0af964
ibfaye <iboufaye2000@hotmail.com>
2021-10-20 17:13:58 -0400

    Initial Structure
    Setup the project initial structure

---
+++ app/src/main/java/com/codebase/cryptocurrencyapp/commun/Constants.kt
@@ -0,0 +1,3 @@
+package com.codebase.cryptocurrencyapp.commun
+
+object Constants {}
--- app/src/test/java/com/codebase/cryptocurrencyapp/ExampleUnitTest.kt
+++ app/src/test/java/com/codebase/cryptocurrencyapp/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.plcoding.cryptocurrencyappyt
+package com.codebase.cryptocurrencyapp
 
 import org.junit.Test
 
